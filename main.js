042060f99ee9a60f18845b4421dc0a3b249ecb32
mick <figura.m@web.de>
2019-04-23 21:37:40 +0200

    test

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ const ClientGame = require("./client/clientgame");
 const GameplayScene = require("./client/gameplayscene");
 const io = require("socket.io-client");
 
-window.onload = () => {
-  var socket = io();
-  /* socket.on('init', function (data) {
-    console.log(data);
-  });
-*/
-  const client = new ClientGame();
-  client.start();
+import Test from "./client/views/test.svelte";
 
-  client.setScene(new GameplayScene(socket));
+window.onload = () => {
+  /* var socket = io();
+   const client = new ClientGame();
+   client.start();
+
+   client.setScene(new GameplayScene(socket));*/
+
+  new Test({
+    target: document.body
+  });
 };
